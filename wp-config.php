f5a224249a821f767a5291b7ac19727c85cdff31
Max <cesare.brandini@gmail.com>
2025-12-02 10:29:06 +0000

    feat: Add `wp-config.php` for WordPress environment variable configuration and mount it in `docker-compose.yml`.

---
+++ wp-config.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The base configuration for WordPress
+ */
+
+// ** Database settings ** //
+define( 'DB_NAME', getenv('WORDPRESS_DB_NAME') );
+define( 'DB_USER', getenv('WORDPRESS_DB_USER') );
+define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
+define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+// ** Authentication Unique Keys and Salts ** //
+define('AUTH_KEY',         getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY',        getenv('NONCE_KEY'));
+define('AUTH_SALT',        getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT',       getenv('NONCE_SALT'));
+
+// ** Database Table prefix ** //
+$table_prefix = getenv('WORDPRESS_TABLE_PREFIX') ?: 'wp_';
+
+// ** Debugging mode ** //
+define( 'WP_DEBUG', !!getenv('WORDPRESS_DEBUG') );
+
+// ** SSL Fix for Reverse Proxy ** //
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
+    $_SERVER['HTTPS'] = 'on';
+}
+
+// ** Force Site URL and Home ** //
+define( 'WP_HOME', 'https://api.debrandstudio.it' );
+define( 'WP_SITEURL', 'https://api.debrandstudio.it' );
+
+define('FORCE_SSL_ADMIN', true);
+define('FORCE_SSL_LOGIN', true);
+
+// ** Redis Configuration ** //
+if (getenv('WP_REDIS_HOST')) {
+    define('WP_REDIS_HOST', getenv('WP_REDIS_HOST'));
+    define('WP_REDIS_PORT', getenv('WP_REDIS_PORT'));
+    define('WP_REDIS_DATABASE', getenv('WP_REDIS_DATABASE'));
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
